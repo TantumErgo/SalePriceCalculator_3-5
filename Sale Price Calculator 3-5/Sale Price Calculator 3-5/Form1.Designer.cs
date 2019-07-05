6d8d532f4458249237c6ded46326835a1e6deaef
TantumErgo <9907196+TantumErgo@users.noreply.github.com>
2019-07-05 17:04:25 -0400

    Upload project files

---
+++ Sale Price Calculator 3-5/Sale Price Calculator 3-5/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Sale_Price_Calculator_3_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.fiveCentsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tenCentsPictureBox = new System.Windows.Forms.PictureBox();
+            this.twentyFiveCentsPictureBox = new System.Windows.Forms.PictureBox();
+            this.fiftyCentsPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fiveCentsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenCentsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twentyFiveCentsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fiftyCentsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(203, 9);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(110, 16);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Click the Coins";
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(332, 93);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(44, 16);
+            this.outputDescriptionLabel.TabIndex = 1;
+            this.outputDescriptionLabel.Text = "Total";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalLabel.Location = new System.Drawing.Point(308, 145);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalLabel.TabIndex = 2;
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(196, 230);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // fiveCentsPictureBox
+            // 
+            this.fiveCentsPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fiveCentsPictureBox.Location = new System.Drawing.Point(3, 32);
+            this.fiveCentsPictureBox.Name = "fiveCentsPictureBox";
+            this.fiveCentsPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.fiveCentsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.fiveCentsPictureBox.TabIndex = 4;
+            this.fiveCentsPictureBox.TabStop = false;
+            this.fiveCentsPictureBox.Click += new System.EventHandler(this.fiveCentsPictureBox_Click);
+            // 
+            // tenCentsPictureBox
+            // 
+            this.tenCentsPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tenCentsPictureBox.Location = new System.Drawing.Point(138, 32);
+            this.tenCentsPictureBox.Name = "tenCentsPictureBox";
+            this.tenCentsPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.tenCentsPictureBox.TabIndex = 5;
+            this.tenCentsPictureBox.TabStop = false;
+            this.tenCentsPictureBox.Click += new System.EventHandler(this.tenCentsPictureBox_Click);
+            // 
+            // twentyFiveCentsPictureBox
+            // 
+            this.twentyFiveCentsPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.twentyFiveCentsPictureBox.Location = new System.Drawing.Point(3, 145);
+            this.twentyFiveCentsPictureBox.Name = "twentyFiveCentsPictureBox";
+            this.twentyFiveCentsPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.twentyFiveCentsPictureBox.TabIndex = 6;
+            this.twentyFiveCentsPictureBox.TabStop = false;
+            this.twentyFiveCentsPictureBox.Click += new System.EventHandler(this.twentyFiveCentsPictureBox_Click);
+            // 
+            // fiftyCentsPictureBox
+            // 
+            this.fiftyCentsPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fiftyCentsPictureBox.Location = new System.Drawing.Point(134, 145);
+            this.fiftyCentsPictureBox.Name = "fiftyCentsPictureBox";
+            this.fiftyCentsPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.fiftyCentsPictureBox.TabIndex = 7;
+            this.fiftyCentsPictureBox.TabStop = false;
+            this.fiftyCentsPictureBox.Click += new System.EventHandler(this.fiftyCentsPictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 265);
+            this.Controls.Add(this.fiftyCentsPictureBox);
+            this.Controls.Add(this.twentyFiveCentsPictureBox);
+            this.Controls.Add(this.tenCentsPictureBox);
+            this.Controls.Add(this.fiveCentsPictureBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.instructionLabel);
+            this.Name = "Form1";
+            this.Text = "Change Counter";
+            ((System.ComponentModel.ISupportInitialize)(this.fiveCentsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenCentsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twentyFiveCentsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fiftyCentsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox fiveCentsPictureBox;
+        private System.Windows.Forms.PictureBox tenCentsPictureBox;
+        private System.Windows.Forms.PictureBox twentyFiveCentsPictureBox;
+        private System.Windows.Forms.PictureBox fiftyCentsPictureBox;
+    }
+}
+
